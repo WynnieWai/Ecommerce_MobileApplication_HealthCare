de095fc82276dbd9ae460ebea7724aa1bcf3bc0d
chinkokfei <ckokfei14@gmail.com>
2025-05-02 17:46:46 +0800

    Hallo

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:testing_asg1/features/authentication/screens/onboarding/onboarding.dart';
-import 'package:testing_asg1/utils/constants/text_strings.dart';
 import 'package:testing_asg1/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
