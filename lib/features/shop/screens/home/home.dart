762f62c171c83d8496863fceb8642073bd7918c3
wanning10 <19wning@gmail.com>
2025-05-02 20:55:33 +0800

    carousal, wishlist

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:testing_asg1/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:testing_asg1/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:testing_asg1/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:testing_asg1/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:testing_asg1/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:testing_asg1/common/widgets/images/t_rounded_image.dart';
+import 'package:testing_asg1/features/shop/screens/home/widgets/promo_slider.dart';
+import 'package:testing_asg1/utils/constants/colors.dart';
+
+import '../../../../../utils/constants/image_strings.dart';
+import '../../../../../utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -28,14 +38,18 @@ class HomeScreen extends StatelessWidget {
                 ],
               )
             ),
+
+            /// Body -- Tutorial [Section # 3, Video # 5]
+            Padding(
+              padding: EdgeInsets.all(TSizes.defaultSpace),
+              child: TPromoSlider(banners: [TImages.promoBanner1, TImages.promoBanner2, TImages.promoBanner3],),
+            )
           ],
         ),
       ),
     );
   }
 }
-
-
 
 
 
--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -3,6 +3,7 @@ import 'package:get/get_instance/get_instance.dart';
 import 'package:get/state_manager.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:testing_asg1/features/shop/screens/home/home.dart';
+import 'package:testing_asg1/features/shop/screens/wishlist/wishlist.dart';
 import 'package:testing_asg1/utils/constants/colors.dart';
 import 'package:testing_asg1/utils/helpers/helper_functions.dart';
 
@@ -42,5 +43,5 @@ class NavigationMenu extends StatelessWidget {
 class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
 
-  final screens = [ const HomeScreen(), Container(color: Colors.purple), Container(color: Colors.orange), Container(color: Colors.blue)];
+  final screens = [ const HomeScreen(), Container(color: Colors.purple), const FavouriteScreen(), Container(color: Colors.blue)];
 }
