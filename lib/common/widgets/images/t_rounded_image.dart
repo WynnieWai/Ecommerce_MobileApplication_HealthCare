f87dd25fc68d5027ebf69e96336b6c0cc3b60e6d
chinkokfei <ckokfei14@gmail.com>
2025-05-02 20:54:24 +0800

    Rounded

--- lib/common/widgets/images/t_rounded_image.dart
+++ lib/common/widgets/images/t_rounded_image.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:testing_asg1/utils/constants/colors.dart';
+import 'package:testing_asg1/utils/constants/sizes.dart';
 
 class TRoundedImage extends StatelessWidget{
   const TRoundedImage({
-    super.key
+    super.key,
+    this.border,
+    this.padding,
+    this.onPressed,
+    this.width,
+    this.height,
+    this.applyImageRadius=true,
+    required this.imageUrl,
+    this.fit=BoxFit.contain,
+    this.backgroundColor=TColors.light,
+    this.isNetworkImage=false,
+    this.borderRadius=TSizes.md,
   });
 
+  final double? width, height;
+  final String imageUrl;
+  final bool applyImageRadius;
+  final BoxBorder? border;
+  final Color backgroundColor;
+  final BoxFit? fit;
+  final EdgeInsetsGeometry? padding;
+  final bool isNetworkImage;
+  final VoidCallback? onPressed;
+  final double borderRadius;
   @override
   Widget build(BuildContext context){
     return GestureDetector(
