a32159e42f5854ccd40ff26e1edf42c667b625ad
chinkokfei <ckokfei14@gmail.com>
2025-05-02 18:52:32 +0800

    setting

--- lib/features/shop/screens/home/widgets/home.dart
+++ lib/features/shop/screens/home/widgets/home.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:testing_asg1/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:testing_asg1/common/widgets/custom_shapes/containers/primary_header_container.dart';
-import 'package:testing_asg1/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
-import 'package:testing_asg1/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import 'package:testing_asg1/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
