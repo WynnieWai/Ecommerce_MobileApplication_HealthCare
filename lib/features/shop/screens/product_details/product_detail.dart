eee15b843694dd31cabeba0ed919566d6353dca1
WynnieWai <waipuishan03@gmail.com>
2025-05-03 21:34:29 +0800

    all changes are merged

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:readmore/readmore.dart';
 import 'package:testing_asg1/common/widgets/appbar/appbar.dart';
@@ -8,6 +9,7 @@ import 'package:testing_asg1/common/widgets/images/t_rounded_image.dart';
 import 'package:testing_asg1/common/widgets/texts/section_heading.dart';
 import 'package:testing_asg1/features/shop/screens/product_details/widgets/bottom_add_to_cart_widget.dart';
 import 'package:testing_asg1/features/shop/screens/product_details/widgets/product_attributes.dart';
+import 'package:testing_asg1/features/shop/screens/product_reviews/product_reviews.dart';
 import 'package:testing_asg1/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:testing_asg1/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:testing_asg1/features/shop/screens/product_details/widgets/rating_share_widget.dart';
@@ -69,7 +71,7 @@ class ProductDetailScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       const TSectionHeading(title: 'Reviews(199)', showActionButton: false),
-                      IconButton(icon: const Icon(Iconsax.arrow_right_3, size: 18), onPressed: (){}),
+                      IconButton(icon: const Icon(Iconsax.arrow_right_3, size: 18), onPressed: () => Get.to(() => const ProductReviewsScreen())),
                     ],
                   ),
                   const SizedBox(height: TSizes.spaceBtwSections),
