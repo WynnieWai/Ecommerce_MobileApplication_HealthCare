771a77ae1dceba5c7574da3f32dfa34ac074a436
WynnieWai <waipuishan03@gmail.com>
2025-05-03 13:59:45 +0800

    video31 changes

---
+++ lib/features/shop/screens/brand/all_brands.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:testing_asg1/common/widgets/appbar/appbar.dart';
+import 'package:testing_asg1/common/widgets/brands/brand_card.dart';
+import 'package:testing_asg1/common/widgets/layouts/grid_layout.dart';
+import 'package:testing_asg1/common/widgets/texts/section_heading.dart';
+import 'package:testing_asg1/utils/constants/sizes.dart';
+
+class AllBrandsScreen extends StatelessWidget{
+  const AllBrandsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const TAppBar(title:Text('Brand'),showBackArrow:true),
+      body:SingleChildScrollView(
+        child:Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child:Column(
+            children: [
+              //Heading
+              const TSectionHeading(title: "Brands",showActionButton: false),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              //Brands
+              TGridLayout(itemCount: 10, mainAxisExtent: 80,itemBuilder:(context,index)=>const TBrandCard(showBorder:true)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
