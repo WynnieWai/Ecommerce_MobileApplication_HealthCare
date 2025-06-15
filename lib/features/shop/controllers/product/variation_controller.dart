0b074fc7ac073f4c3cd9731655bc90b8a6f79667
GuangDe7822 <guangde7822@gmail.com>
2025-06-15 23:56:21 +0800

    fetch product from direbase, and utilise firebase details in Product Detail Screen

---
+++ lib/features/shop/controllers/product/variation_controller.dart
@@ -0,0 +1,82 @@
+import 'package:get/get.dart';
+import 'package:testing_asg1/features/shop/controllers/product/images_controller.dart';
+import 'package:testing_asg1/features/shop/models/product_model.dart';
+import 'package:testing_asg1/features/shop/models/product_variation_model.dart';
+
+class VariationController extends GetxController {
+  static VariationController get instance => Get.find();
+
+  // Variables
+  RxMap selectedAttributes = {}.obs;
+  RxString variationStockStatus = ''.obs;
+  Rx<ProductVariationModel> selectedVariation = ProductVariationModel.empty().obs;
+
+  // Select Attribute, and Variation
+  void onAttributeSelected(ProductModel product, attributeName, attributeValue) {
+    // When attribute is selected we will first add that attribute to the selectedAttributes
+    final selectedAttributes = Map<String, dynamic>.from(this.selectedAttributes);
+    selectedAttributes[attributeName] = attributeValue;
+    this.selectedAttributes[attributeName] = attributeValue;
+
+    final selectedVariation = product.productVariations!.firstWhere(
+      (variation) => _isSameAttributesValues(variation.attributeValues, selectedAttributes),
+      orElse: () => ProductVariationModel.empty(),
+    );
+
+    // Show the selected variation image as a Main Image
+    if (selectedVariation.image.isNotEmpty) {
+      ImagesController.instance.selectedProductImage.value =
+          selectedVariation.image;
+    }
+
+    // Assign the Selected Variation
+    this.selectedVariation.value = selectedVariation;
+  }
+
+  // Check if selected attributes matches any variation attributes
+  bool _isSameAttributesValues(Map<String, dynamic> variationAttributes, Map<String, dynamic> selectedAttributes) {
+    //  If selected attributes contains 3 attributes and current variation contains 2 then return.
+    if (variationAttributes.length != selectedAttributes.length) return false;
+
+    // If any of the attributes is different then return .e.g. [Green, Large] x [Green, Small]
+    for (final key in variationAttributes.keys) {
+      //  Attributes[Key] = value which could be [ Green, Small, Cotton] etc.
+      if (variationAttributes[key] != selectedAttributes[key]) return false;
+    }
+    return true;
+  }
+
+  // Check Attribute availability / Stock in variation
+  Set<String?> getAttributesAvailabilityInVariation(List<ProductVariationModel> variations, String attributeName) {
+    // Pass the variations to check which attributes are available and stock is not 0
+    final availableVariationAttributeValues = variations
+        .where((variation) =>
+            variation.attributeValues[attributeName] != null &&
+            variation.attributeValues[attributeName]!.isNotEmpty &&
+            variation.stock > 0
+        )
+        .map((variation) => variation.attributeValues[attributeName])
+        .toSet();
+
+    return availableVariationAttributeValues;
+  }
+
+  String getVariationPrice() {
+    return (selectedVariation.value.salePrice > 0
+        ? selectedVariation.value.salePrice.toStringAsFixed(2)
+        : selectedVariation.value.price.toStringAsFixed(2))
+        .toString();
+  }
+
+  // Check Product Variation Stock Status
+  void getProductVariationStockStatus() {
+    variationStockStatus.value = selectedVariation.value.stock > 0 ? 'In Stock' : 'Out of Stock';
+  }
+
+  // Reset Selected Attributes when switching products
+  void resetSelectedAttributes() {
+    selectedAttributes.clear();
+    variationStockStatus.value = '';
+    selectedVariation.value = ProductVariationModel.empty();
+  }
+}
