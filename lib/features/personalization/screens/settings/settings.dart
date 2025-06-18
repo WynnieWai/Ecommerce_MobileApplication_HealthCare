3cd21e4bb528c8000d92e8ba357242a4ee6ebad2
chinkokfei <ckokfei14@gmail.com>
2025-06-18 11:17:53 +0800

    Latest

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -14,87 +14,108 @@ import "package:testing_asg1/features/shop/screens/order/order.dart";
 import "package:testing_asg1/utils/constants/colors.dart";
 import "package:testing_asg1/utils/constants/sizes.dart";
 
-class SettingScreen extends StatelessWidget{
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body:SingleChildScrollView(
-        child:Column(
-          children:[
-            TPrimaryHeaderContainer(
-              child:Column(
-                children:[
-                  ///AppBar
-                  TAppBar(title: Text('Account',style: Theme.of(context).textTheme.headlineMedium!.apply(color:TColors.white))),
+  State<SettingScreen> createState() => _SettingScreenState();
+}
 
-                  ///User Profile Card
-                  TUserProfileTile(onPressed: ()=>Get.to(()=>const ProfileScreen())),
-                   
-                  const SizedBox(height:TSizes.spaceBtwSections),
+class _SettingScreenState extends State<SettingScreen> {
+  bool isGeolocationOn = true;
+  bool isSafeModeOn = true;
+  bool isHDImageQualityOn = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  TAppBar(
+                    title: Text(
+                      'Account',
+                      style: Theme.of(context).textTheme.headlineMedium!.apply(color: TColors.white),
+                    ),
+                  ),
+                  TUserProfileTile(onPressed: () => Get.to(() => const ProfileScreen())),
+                  const SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
             ),
-
-
-            /// -- Body
             Padding(
               padding: const EdgeInsets.all(TSizes.defaultSpace),
-              child:Column(
+              child: Column(
                 children: [
-                  // -- Account Settings
-                  const TSectionHeading(title: 'Accont Setting',showActionButton: false,),
+                  const TSectionHeading(title: 'Accont Setting', showActionButton: false),
                   const SizedBox(height: TSizes.spaceBtwItems),
-
-                  TSettingsMenuTile(icon: Iconsax.safe_home, title: 'My Addresses', subTitle: 'Set shopping delivery address',onTap:()=>Get.to(()=>const UserAddressScreen())),
-                  TSettingsMenuTile(icon: Iconsax.shopping_cart, title: 'My Cart', subTitle: 'Add,remove products and move to checkout', onTap: ()=>Get.to(()=>const CartScreen()),),
-                  TSettingsMenuTile(icon: Iconsax.bag_tick, title: 'My Orders', subTitle: 'In-progress and Completed Orders',onTap: ()=>Get.to(()=>const OrderScreen()),),
+                  TSettingsMenuTile(icon: Iconsax.safe_home, title: 'My Addresses', subTitle: 'Set shopping delivery address', onTap: () => Get.to(() => const UserAddressScreen())),
+                  TSettingsMenuTile(icon: Iconsax.shopping_cart, title: 'My Cart', subTitle: 'Add,remove products and move to checkout', onTap: () => Get.to(() => const CartScreen())),
+                  TSettingsMenuTile(icon: Iconsax.bag_tick, title: 'My Orders', subTitle: 'In-progress and Completed Orders', onTap: () => Get.to(() => const OrderScreen())),
                   TSettingsMenuTile(icon: Iconsax.bank, title: 'Bank Account', subTitle: 'Withdraw balance to registered bank account'),
                   TSettingsMenuTile(icon: Iconsax.discount_shape, title: 'My Coupons', subTitle: 'List of all the discounted coupons'),
                   TSettingsMenuTile(icon: Iconsax.notification, title: 'Notifications', subTitle: 'Set any kind of notification message'),
                   TSettingsMenuTile(icon: Iconsax.security_card, title: 'Account Privacy', subTitle: 'Manage data usage and connected accounts'),
 
-                  // -- App Setting
                   SizedBox(height: TSizes.spaceBtwSections),
-                  TSectionHeading(title: 'App Settings',showActionButton: false),
-                  SizedBox(height:TSizes.spaceBtwItems),
+                  TSectionHeading(title: 'App Settings', showActionButton: false),
+                  SizedBox(height: TSizes.spaceBtwItems),
                   TSettingsMenuTile(icon: Iconsax.document_upload, title: 'Load Data', subTitle: 'Upload Data to your Cloud Firebase'),
+
                   TSettingsMenuTile(
-                    icon: Iconsax.location, 
-                    title:'Geolocation', 
+                    icon: Iconsax.location,
+                    title: 'Geolocation',
                     subTitle: 'Set recommendation based on location',
-                    trailing: Switch(value: true, onChanged: (value){}),
+                    trailing: Switch(
+                      value: isGeolocationOn,
+                      onChanged: (value) {
+                        setState(() {
+                          isGeolocationOn = value;
+                        });
+                      },
+                    ),
                   ),
 
                   TSettingsMenuTile(
-                    icon: Iconsax.security_user, 
-                    title:'Safe Mode', 
+                    icon: Iconsax.security_user,
+                    title: 'Safe Mode',
                     subTitle: 'Search result is safe for all pages',
-                    trailing: Switch(value: true, onChanged: (value){}),
+                    trailing: Switch(
+                      value: isSafeModeOn,
+                      onChanged: (value) {
+                        setState(() {
+                          isSafeModeOn = value;
+                        });
+                      },
+                    ),
                   ),
 
                   TSettingsMenuTile(
-                    icon: Iconsax.image, 
-                    title:'HD Image Quality', 
+                    icon: Iconsax.image,
+                    title: 'HD Image Quality',
                     subTitle: 'Set all quality to be seen',
-                    trailing: Switch(value: true, onChanged: (value){}),
+                    trailing: Switch(
+                      value: isHDImageQualityOn,
+                      onChanged: (value) {
+                        setState(() {
+                          isHDImageQualityOn = value;
+                        });
+                      },
+                    ),
                   ),
 
-
-                  // -- Logout Button
-                  const SizedBox(height:TSizes.spaceBtwSections),
+                  const SizedBox(height: TSizes.spaceBtwSections),
                   SizedBox(
                     width: double.infinity,
-                    // child:OutlinedButton(onPressed: (){}, child: const Text('Logout')),
                     child: OutlinedButton(
                       onPressed: () async {
                         try {
                           await AuthenticationRepository.instance.logout();
-                          // Optionally, show a confirmation message
                           Get.snackbar('Logout Successful', 'You have been logged out.', snackPosition: SnackPosition.BOTTOM);
                         } catch (e) {
-                          // Handle any errors during logout
                           Get.snackbar('Logout Failed', e.toString(), snackPosition: SnackPosition.BOTTOM);
                         }
                       },
@@ -102,13 +123,13 @@ class SettingScreen extends StatelessWidget{
                     ),
                   ),
 
-                  const SizedBox(height: TSizes.spaceBtwSections*2.5),
+                  const SizedBox(height: TSizes.spaceBtwSections * 2.5),
                 ],
-              )
-            )
-          ]
-        )
-      )
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
