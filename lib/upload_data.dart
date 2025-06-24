36edda8e03d016377591fd32fd499b29a9daf56b
chinkokfei <ckokfei14@gmail.com>
2025-06-24 23:49:19 +0800

    Latest

--- lib/upload_data.dart
+++ lib/upload_data.dart
@@ -4,48 +4,46 @@ import 'package:flutter/material.dart';
 void uploadDummyProducts() async {
   final firestore = FirebaseFirestore.instance;
     // for (int i = 14; i <= 54; i++) {
-    final productId = '056'; // "014" ~ "054"
+    final productId = '087'; // "014" ~ "054"
 
     final dummyProduct = {
-      'Title': 'Pantene No Rinse Conditioner Perfect On Instasmooth (40ml x 12)',
-      'Stock': 20,
-      'Price': 64.50,
+      'Title': 'Dove Sakura Blossom Body Wash 1L',
+      'Stock': 10,
+      'Price': 27.50,
       'IsFeatured': true,
-      'Thumbnail': 'https://firebasestorage.googleapis.com/v0/b/ecommerceapp-87f00.firebasestorage.app/o/ProductImage%2FHair%20Care%2FHair%20Treatment%2FPantene%2Fpantene-hair-treatment.png?alt=media&token=e8e04586-3b63-494f-85f1-468dba094e1b',
+      'Thumbnail': 'https://firebasestorage.googleapis.com/v0/b/ecommerceapp-87f00.firebasestorage.app/o/ProductImage%2FPersonal%20Care%2FBath%20Care%2FDove%2Fdove-sakura.png?alt=media&token=35b8c5eb-6a2c-496f-a8d9-483e9883f5a2',
       'Brand': {
-        'Id': 'B035',
-        'Image': 'https://firebasestorage.googleapis.com/v0/b/ecommerceapp-87f00.firebasestorage.app/o/BrandLogo%2FPantene%2Fpantene-logo.png?alt=media&token=ad9eb46f-9ad0-40d3-987c-2d49d19cf0ed',
-        'Name': 'Pantene',
-        'ProductsCount': 88,
+        'Id': 'B002',
+        'Image': 'https://firebasestorage.googleapis.com/v0/b/ecommerceapp-87f00.firebasestorage.app/o/BrandLogo%2FDove%2Fdove-logo.jpg?alt=media&token=d5941392-2044-436d-86a5-f3dccca66e5d',
+        'Name': 'Dove',
+        'ProductsCount': 10,
         'IsFeatured': true
       },
       'Images': [
-        'https://firebasestorage.googleapis.com/v0/b/ecommerceapp-87f00.firebasestorage.app/o/ProductImage%2FHair%20Care%2FHair%20Treatment%2FPantene%2Fpantene-hair-treatment.png?alt=media&token=e8e04586-3b63-494f-85f1-468dba094e1b'
+        'https://firebasestorage.googleapis.com/v0/b/ecommerceapp-87f00.firebasestorage.app/o/ProductImage%2FPersonal%20Care%2FBath%20Care%2FDove%2Fdove-sakura.png?alt=media&token=35b8c5eb-6a2c-496f-a8d9-483e9883f5a2'
         
       ],
-      'Description': 'Pantene No Rinse Conditioner Perfect On Instasmooth (40ml x 12) is a lightweight, leave-in conditioner designed for instant smoothness and all-day protection. Infused with Collagen Repair & Smooth technology, it helps tame flyaways, reduce frizz, and keep hair soft and manageable. Ideal for use on damp hair at home before brushing or as an on-the-go touch-up. Use just one coin-sized drop (2ml) per application, applying from mid-length to ends while avoiding the scalp. Not a toy—keep out of reach of children.',
-      'SalePrice':64.50,
-      'SKU': 'SKU056',
-      'CategoryId': 'C019',
+      'Description': 'Dove Sakura Blossom Body Wash 1L is enriched with Japanese-style pinkish sakura and rice water essence to gently cleanse and deeply hydrate the skin. It leaves your skin feeling plump, tender, and subtly scented after each shower. The nourishing formula replenishes moisture, making it ideal for daily use. Simply pour into your hand, lather, massage over the body, and rinse off with water for soft, refreshed skin.',
+      'SalePrice':27.50,
+      'SKU': 'SKU087',
+      'CategoryId': 'C014',
       'ProductType': 'ProductType.variable',
       'ProductAttributes': [
         {
           'Name': 'Size',
-          'Values': ['12 packs']
+          'Values': ['1000ml']
         },
       ],
       'ProductVariations': [
         {
           'Id': '1',
-          'Stock': 88,
-          'Price':64.50,
-          'SKU': '12packs SKU',
-          'SalePrice': 64.50,
-          'Image': 'https://firebasestorage.googleapis.com/v0/b/ecommerceapp-87f00.firebasestorage.app/o/ProductImage%2FHair%20Care%2FHair%20Treatment%2FPantene%2Fpantene-hair-treatment.png?alt=media&token=e8e04586-3b63-494f-85f1-468dba094e1b',
-          'Description': 'Pantene No Rinse Conditioner Perfect On Instasmooth (40ml x 12) is a lightweight, leave-in conditioner designed for instant smoothness and all-day protection. Infused with Collagen Repair & Smooth technology, it helps tame flyaways, reduce frizz, and keep hair soft and manageable. Ideal for use on damp hair at home before brushing or as an on-the-go touch-up. Use just one coin-sized drop (2ml) per application, applying from mid-length to ends while avoiding the scalp. Not a toy—keep out of reach of children.',
-          'AttributeValues': {'Size': '12 packs'}
-        },
-         
+          'Stock': 10,
+          'Price':27.50,
+          'SKU': '1000ml SKU',
+          'SalePrice': 27.50,
+          'Image': 'https://firebasestorage.googleapis.com/v0/b/ecommerceapp-87f00.firebasestorage.app/o/ProductImage%2FPersonal%20Care%2FBath%20Care%2FDove%2Fdove-sakura.png?alt=media&token=35b8c5eb-6a2c-496f-a8d9-483e9883f5a2',
+          'AttributeValues': {'Size': '1000ml'}
+        }
       ]
     };
 
